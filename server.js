64323df2651c5e57655c1bc10abb29aac08fd035
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-11-21 01:07:03 +0100

    :recycle: refactor: Simplificar aquivo do servidor

--- server.js
+++ server.js
@@ -1,49 +1,10 @@
 import express from "express";
 import conectarAoBanco from "./src/config/dbconfig.js";
+import routes from "./src/routes/postsRoutes.js";
 
-
-const conexao = await conectarAoBanco(process.env.STRING_DE_CONEXAO);
 const app = express();
+routes(app);
 const PORT = 3000;
-
-const posts = [
-  {
-    id: 1,
-    descricao: "Foto teste",
-    imgUrl: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 2,
-    descricao: "Gato preguiçoso tomando sol",
-    imgUrl: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 3,
-    descricao: "Gatinho curioso explorando uma caixa",
-    imgUrl: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 4,
-    descricao: "Gata brincando com um novelo de lã",
-    imgUrl: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 5,
-    descricao: "Gato com olhos azuis esverdeados",
-    imgUrl: "https://placecats.com/millie/300/150",
-  },
-  {
-    id: 6,
-    descricao: "Grupo de gatinhos brincando",
-    imagem: "https://placecats.com/millie/300/150",
-  },
-];
-
-async function getTodosPosts() {
-  const db = conexao.db("imersao-instalikes");
-  const coleccao = db.collection("posts");
-  return coleccao.find().toArray();
-}
 
 function getPostById(id) {
   return posts.findIndex((post) => post.id === Number(id));
@@ -51,10 +12,6 @@ function getPostById(id) {
 
 app.use(express.json());
 
-app.get("/posts", async (req, res) => {
-  const posts = await getTodosPosts();
-  res.status(200).json(posts);
-});
 
 app.get("/posts/:id", (req, res) => {
   const index = getPostById(req.params.id);
