555e47563de13f07cbf6f3eca30d3592c3e0509a
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-11-26 10:50:25 +0100

    :sparkles: feat: Adicionar postController

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,6 +1,13 @@
-import { getTodosPosts } from "../models/postModel.js";
+import { getTodosPosts, addUser } from "../models/postModel.js";
 
 export async function listarPosts(req, res) {
   const posts = await getTodosPosts();
   res.status(200).json(posts);
+}
+
+export async function addUsers(req, res) {
+  const { name, email, age } = req.body;
+  const user = { name, email, age };
+  const resultado = await addUser(user);
+  res.status(201).json({message: "User created!", user: resultado});
 }
