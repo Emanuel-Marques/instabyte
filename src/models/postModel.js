e32cbb5fb9a0bad4ea20b1532ac3848bf703595b
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-11-26 22:10:34 +0100

    :sparkles: feat: Adicionar função para actualizar post no model

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -1,4 +1,6 @@
+import "dotenv/config"
 import conectarAoBanco from "../config/dbconfig.js";
+import { ObjectId } from "mongodb";
 
 const conexao = await conectarAoBanco(process.env.STRING_DE_CONEXAO);
 
@@ -13,6 +15,13 @@ export async function criarPost(newPost){
   const coleccao = db.collection("posts");
   const post = newPost;
   return coleccao.insertOne(post);
+}
+
+export async function actualizarPost(id, post){
+  const db = conexao.db("imersao-instalikes");
+  const coleccao = db.collection("posts");
+  const objId = ObjectId.createFromHexString(id); // Convertendo o id em um objecto que o MongoDB entende
+  return coleccao.updateOne({ _id: new ObjectId(objId) }, {$set: post} ) // Actualizando dados no MongoDB
 }
 
 export async function addUser({name, email, age}){
