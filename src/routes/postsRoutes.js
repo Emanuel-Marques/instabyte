4241640f96acb74728e8b9836b43ee87a1e89d20
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-11-26 22:09:24 +0100

    :sparkles: feat: Configurar cors e adicionar rota para actualizar post

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,6 +1,12 @@
 import express from "express";
 import multer from "multer";
-import { addUsers, listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+import cors from "cors";
+import { actualizaNovoPost, addUsers, listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+const corsOptions = {
+  origin: "http://localhost:8000",
+  optionsSucessStatus: 200 
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -14,9 +20,12 @@ const upload = multer({ dest: "./uploads", storage });
 
 const routes = (app) => {
   app.use(express.json());
-
+  app.use(cors(corsOptions));
   app.get("/posts", listarPosts);
+
   app.post("/posts", postarNovoPost);
+
+  app.put("/upload/:id", actualizaNovoPost);
   app.post("/upload", upload.single("imagem"), uploadImagem);
   app.post("/users", addUsers);
 };
