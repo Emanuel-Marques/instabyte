c095ba7673266bf9391a6ee0478ec2d8c74edc2c
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-11-26 20:27:13 +0100

    :sparkles: feat: Adicionar rota para criar post e fazer upload de imagem

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,23 @@
 import express from "express";
-import { addUsers, listarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+import { addUsers, listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+})
+const upload = multer({ dest: "./uploads", storage });
 
 const routes = (app) => {
   app.use(express.json());
 
   app.get("/posts", listarPosts);
+  app.post("/posts", postarNovoPost);
+  app.post("/upload", upload.single("imagem"), uploadImagem);
   app.post("/users", addUsers);
 };
 
